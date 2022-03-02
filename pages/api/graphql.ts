df8f5192d82c21cb46dc666d36801bfcf62df111
hateVtubers <gmolloflores@gmail.com>
2022-03-02 01:27:01 -0300

    try fixed bug in graphql api

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,28 +1,29 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { ApolloServer } from 'apollo-server-micro';
-import { typeDefs } from '../../graphql/typeDefs';
-import { resolvers } from '../../graphql/resolvers';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ApolloServer } from "apollo-server-micro";
+import { typeDefs } from "../../graphql/typeDefs";
+import { resolvers } from "../../graphql/resolvers";
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
 });
 
 const startServer = apolloServer.start();
 
 const graphqlServer = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
 
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     res.end();
     return false;
   }
 
   await startServer;
   await apolloServer.createHandler({
-    path: '/api/graphql',
+    path: "/api/graphql",
   })(req, res);
 };
 
